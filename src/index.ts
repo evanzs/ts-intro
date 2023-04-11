cef6e609d075c38e34008ba11c6aac8ac9454fb3
evanzs <evandrofidk@hotmail.com>
2023-04-11 14:08:32 -0300

    using function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
+function somar(a:number,b:number):number{
+    return a+b;
+}
 
-console.log('Olá Mundo!');
+const somarFlecha  = (a:number,b:number):number =>{
+    return a+b;
+}
+
+function mult (numero:number,outro?:number,base:number =2){
+    return numero*base;
+}
+
+const result = somar(2,3) 
+const somarFlechaResult = somarFlecha(2,3)
+const multResult = mult(2,3)
 
 
+console.log(result,somarFlechaResult,multResult)
